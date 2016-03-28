90a4c6024a50b485f530d9a4a3ed51427661fbd7
Soojung Im <sjj3747@naver.com>
2016-03-28 20:14:05 +0900

    #2 add Result Activity

---
+++ app/src/main/java/kr/hs/emirim/sjung/monchicken/ResultActivity.java
@@ -0,0 +1,17 @@
+package kr.hs.emirim.sjung.monchicken;
+
+//import android.app.Notification;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by appcreator14 on 2016. 3. 28..
+ */
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
