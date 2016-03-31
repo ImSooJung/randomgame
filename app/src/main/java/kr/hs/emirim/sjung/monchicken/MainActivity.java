bc5197109411c3b3929a7b5a394a5c42e96c7b72
Soojung Im <sjj3747@naver.com>
2016-03-31 20:59:02 +0900

    #4 add button & onClicklistener

--- app/src/main/java/kr/hs/emirim/sjung/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sjung/monchicken/MainActivity.java
@@ -2,24 +2,42 @@ package kr.hs.emirim.sjung.monchicken;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText mName;
+    Button mButton;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName = (EditText)findViewById(R.id.name);
-
+        mName = (EditText) findViewById(R.id.name);
+        mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
+        mButton.setOnClickListener(this);//버튼 클
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mName.setText("");
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override//어노테이션?
+    public void onClick(View v) {
+        Toast.makeText(this,"집에가고싶다!",Toast.LENGTH_LONG).show();//현재액티비티로넘어감(duration:기간-띄워주는....상수형으로적는다(대문자))
+
     }
 }
